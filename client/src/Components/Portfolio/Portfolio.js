8d3c026c113bab6948259e150357ac4ccc6a6c3b
Joel Delatte <joeldelatte@gmail.com>
2020-05-04 20:38:36 -0500

    adds handleSubmit to contact form and config to root

--- client/src/Components/Portfolio/Portfolio.js
+++ client/src/Components/Portfolio/Portfolio.js
@@ -1,13 +1,5 @@
 import React, {useState} from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   useRouteMatch,
-//   useParams
-// } from "react-router-dom";
-// import PortfolioItem from "./Items/PortfolioItem"
+
 import Dayplanner from "../Portfolio/images/dayplanner.png";
 import NewsPaper from "../Portfolio/images/austin1.jpeg";
 import Burger from "../Portfolio/images/burger.jpg"
@@ -16,19 +8,17 @@ import Knife from "../Portfolio/images/knife.png"
 import './style.css';
 
 export default function Portfolio() {
-    // const match = useRouteMatch();
-    const [modal1, setModal1] = useState(false);
+
 
     return (
         <>
         <div className="viewContainer">
             <h1>Here are some of my projects...</h1>
-                <div className="projectBox"  > {/* onMouseOver={()=> setModal1(true)} onMouseOverCapture ={setModal1(false)} */}
+                <div className="projectBox"  >
                   <a href="https://rocky-meadow-16617.herokuapp.com/">
                     <img class="projectPhoto" src={Knife}></img> 
                     <section className="projectLabel">Dinner Party</section>
                   </a>
-                  {/* {(modal1 && <p>Dinner Party is an event planning app where users can create invitations and rsvp to others parties.</p> )}           */}
                 </div>
                 <div className="projectBox">
                   <a href="https://mmoritz8.github.io/Project-1/Joel_art/arts.html">
@@ -53,8 +43,3 @@ export default function Portfolio() {
     )
 }
 
-            // <div className="portfolioNav">
-            //     <Link to={`${match.url}/dayplanner`} >Day Planner</Link>
-            //     <Link to={`${match.url}/dinnerparty`} >Dinner Party</Link>
-            //     <Link to={`${match.url}/austintribune`} >Austin Tribune</Link>
-            // </div>

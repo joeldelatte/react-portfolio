f467d7506f7b555a8db82decd737457f78b933de
Joel Delatte <joeldelatte@gmail.com>
2020-05-06 13:25:18 -0500

    deletes unnneeded empty component

---
+++ .history/server_20200504215423.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 3001;
+const app = express();
+var index = require('./routes/index');
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    USER: process.env.REACT_APP_YOUR_EMAIL_ADDRESS,
-    PASS: process.env.REACT_APP_EMAIL_PASSWORD
+    USER: 'YOUR_EMAIL_ADDRESS',
+    PASS: 'EMAIL_PASSWORD'
 }
